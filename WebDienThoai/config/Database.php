952c982e6fe46ad761b04e49be9628098432d571
Nguyễn Hoàng Hưng <weew3426@gmail.com>
2025-12-14 13:46:13 +0700

    cau truc moi

--- WebDienThoai/config/Database.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');      
-define('DB_PASS', '12345678');          
-define('DB_NAME', 'theking_mobile'); 
-define('DB_CHARSET', 'utf8mb4');
-?>
